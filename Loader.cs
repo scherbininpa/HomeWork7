663ff2c598b289ea07e0610345ea43298fed7720
pa.shcherbinin <shcherbininpa@gmail.com>
2022-07-12 10:47:00 +0500

    Create struct Loader

---
+++ Loader.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork7
+{
+    struct Loader
+    {
+        /// <summary>
+        /// разделитель полей в файле
+        /// </summary>
+        private const string Separator="#";
+        private string Path;
+        /// <summary>
+        /// Структура для работы с файлом Пользователей
+        /// </summary>
+        /// <param name="Path"></param>
+        public Loader(string Path)
+        {
+            this.Path=Path;
+        }
+    }
+}
