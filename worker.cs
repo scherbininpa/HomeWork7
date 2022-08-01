22e13835db64d5038aaaaa2007ed3ca34612f03e
pa.shcherbinin <shcherbininpa@gmail.com>
2022-08-01 06:52:31 +0500

    create new Struct worker

---
+++ worker.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork7
+{
+    struct worker
+    {
+        public int Id { get; set; }
+        public string FIO { get; set; }
+        public int Age { get; set; }
+        public int Height { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public DateTime PlaceOfBirth { get; set; }
+    }
+}
